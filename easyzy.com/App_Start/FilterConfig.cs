5e5ab7abbaf32c13a51954b8cff26dceaff5008a
jiang yuanqing <jiang_yuanqing@126.com>
2017-11-08 13:46:22 +0800

    提交

--- easyzy.com/App_Start/FilterConfig.cs
+++ easyzy.com/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using easyzy.common;
+using System.Web;
 using System.Web.Mvc;
 
 namespace easyzy.com
@@ -7,7 +8,32 @@ namespace easyzy.com
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ExceptionFilterAttribute());
+
+        }
+
+        /// <summary>
+        /// 统一日志处理
+        /// </summary>
+        public class ExceptionFilterAttribute : HandleErrorAttribute
+        {
+            public override void OnException(ExceptionContext filterContext)
+            {
+                var path = $"{filterContext.RouteData.GetRequiredString("controller")}/{filterContext.RouteData.GetRequiredString("action")}";
+                var message = $"消息类型：{filterContext.Exception.GetType().Name}\r\n" +
+                                $"消息内容：{filterContext.Exception.Message}\r\n" +
+                                $"异常方法：{filterContext.Exception.TargetSite}\r\n" +
+                                $"异常对象：{filterContext.Exception.Source}\r\n" +
+                                $"异常目录：{path}\r\n" +
+                                $"堆栈信息: {filterContext.Exception.StackTrace}";
+                var para = (HttpRequestWrapper)((HttpContextWrapper)filterContext.HttpContext).Request;
+                var context = HttpContext.Current.Request.UserAgent;
+                //var userId = int.Parse(string.IsNullOrEmpty(Util.GetCookie("UserID")) ? "0" : Util.GetCookie("UserID"));
+                var userParam = $"\r\n异常用户:{0}\r\n请求设备:{context}\r\n异常路由:{para.Url}";
+
+                LogHelper.Error(message + userParam);
+                base.OnException(filterContext);
+            }
         }
     }
 }
---
+++ easyzy.com/Controllers/ZyController.cs
@@ -0,0 +1,18 @@
+﻿using easyzy.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace easyzy.com.Controllers
+{
+    public class ZyController : Controller
+    {
+        public ActionResult add()
+        {
+            
+            return View();
+        }
+    }
+}
