a63822a7f1d913c1b48da74fd6a31ecc572ef561
jiang yuanqing <jiang_yuanqing@126.com>
2017-11-28 11:09:31 +0800

    添加user.easyzy.com

---
+++ user.easyzy.com/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace user.easyzy.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Reg()
+        {
+            return View();
+        }
+    }
+}
---
+++ user.easyzy.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="user.easyzy.com.MvcApplication" Language="C#" %>
