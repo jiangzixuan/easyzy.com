6216720c95c6db55d5676783a46e7db63d00c5c0
jiang yuanqing <jiang_yuanqing@126.com>
2017-12-05 13:57:37 +0800

    添加m.easyzy.com

---
+++ m.easyzy.com/Controllers/CommonController.cs
@@ -0,0 +1,41 @@
+﻿using easyzy.bll;
+using easyzy.common;
+using easyzy.model.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace m.easyzy.com.Controllers
+{
+    /// <summary>
+    /// 公共功能
+    /// </summary>
+    public class CommonController : Controller
+    {
+        protected int UserId = 0;
+        protected T_User User = null;
+
+        public CommonController()
+        {
+            string u = Util.GetCookie(EasyzyConst.CookieName_User, EasyzyConst.CookieVluew_UserId);
+            if (!string.IsNullOrEmpty(u))
+            {
+                UserId = int.Parse(u);
+                User = B_UserRedis.GetUser(UserId);
+            }
+            ViewBag.UserInfo = User;
+        }
+
+        public void Exit()
+        {
+            try
+            {
+                Util.ClearCookies(EasyzyConst.CookieName_User);
+            }
+            catch
+            { }
+        }
+    }
+}
