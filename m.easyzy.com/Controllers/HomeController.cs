e8ebcd5ec5be46a82be93635364a19a44827b0f7
jiang yuanqing <jiang_yuanqing@126.com>
2017-12-06 13:42:50 +0800

    手机端提交作业

--- m.easyzy.com/Controllers/HomeController.cs
+++ m.easyzy.com/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using easyzy.bll;
 using easyzy.common;
+using easyzy.model.dto;
 using easyzy.model.entity;
 using Newtonsoft.Json;
 using System;
@@ -43,6 +44,67 @@ namespace m.easyzy.com.Controllers
             }
 
             return PartialView(zys);
+        }
+
+        /// <summary>
+        /// 提交答案
+        /// </summary>
+        /// <param name="zyNum"></param>
+        /// <param name="trueName"></param>
+        /// <param name="objecttiveAnswer"></param>
+        /// <param name="imgAnswer"></param>
+        /// <returns></returns>
+        public string SubmitAnswer(string zyNum, string trueName, string objecttiveAnswer, string imgAnswer)
+        {
+            if (string.IsNullOrEmpty(trueName))
+            {
+                return "1|真实姓名未填写！";
+            }
+
+            int zyId = EasyzyConst.GetZyId(zyNum);
+
+            if (B_Zy.IsZySubmited(zyId, trueName))
+            {
+                return "1|此姓名已提交过作业！";
+            }
+
+            List<dto_Answer> al = new List<dto_Answer>();
+            dto_Answer a = null;
+            if (!string.IsNullOrEmpty(objecttiveAnswer))
+            {
+                List<T_ZyStruct> zysl = B_ZyRedis.GetZyStruct(zyId);
+                string[] ol = objecttiveAnswer.Split('|');
+                foreach (var o in ol)
+                {
+                    string[] oc = o.Split(',');
+                    a = new dto_Answer();
+                    a.StructId = int.Parse(oc[0]);
+                    T_ZyStruct zys = zysl.First(s => s.Id == a.StructId);
+                    a.BqNum = zys.BqNum;
+                    a.SqNum = zys.SqNum;
+                    a.Answer = oc[1];
+                    al.Add(a);
+                }
+            }
+
+            T_Answer t = new T_Answer()
+            {
+                ZyId = zyId,
+                StudentId = UserId,
+                TrueName = trueName,
+                AnswerJson = JsonConvert.SerializeObject(al),
+                AnswerImg = imgAnswer,
+                CreateDate = DateTime.Now,
+                Ip = ClientUtil.Ip,
+                IMEI = ClientUtil.IMEI,
+                MobileBrand = ClientUtil.MobileBrand,
+                SystemType = Request.Browser.Platform.ToString(),
+                Browser = Request.Browser.Browser.ToString()
+            };
+
+            int i = B_Zy.AddZyAnswer(t);
+
+            return i > 0 ? ("0|" + B_ZyRedis.GetZy(zyId).AnswerHtmlPath) : "1|提交入库失败！";
         }
     }
 }
---
+++ m.easyzy.com/Scripts/m.easyzy.common.js
@@ -0,0 +1,39 @@
+﻿/* 以下是一些公共方法 */
+function dialogAlert(content) {
+    dialog({
+        title: '系统提示',
+        content: content
+    }).showModal();
+}
+
+function dialogAlertNoModal(content) {
+    dialog({
+        title: '系统提示',
+        content: content
+    }).show();
+}
+
+function dialogConfirm(content, okfunction, cancelfunction) {
+    var d = dialog({
+        title: '系统提示',
+        content: content,
+        okValue: '确定',
+        ok: okfunction,
+        cancelValue: '取消',
+        cancel: (cancelfunction == '' ? 'function () {}' : cancelfunction)
+    }).showModal();
+
+    return d;
+}
+
+function trim(str) {   //删除左右两端的空格
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+function ltrim(str) { //删除左边的空格
+    return str.replace(/(^\s*)/g, "");
+}
+
+function rtrim(str) { //删除右边的空格
+    return str.replace(/(\s*$)/g, "");
+}
