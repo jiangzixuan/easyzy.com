5480d00fe6ebcac99337204a3c07df19a317ed21
jiang yuanqing <jiang_yuanqing@126.com>
2017-12-15 17:36:01 +0800

    提交

--- userthrift.server/User.cs
+++ userthrift.server/User.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Thrift;
+using Thrift.Protocol;
+using Thrift.Transport;
 using userthrift.itf;
+using userthrift.itf.model;
 
 namespace userthrift.server
 {
@@ -16,6 +20,27 @@ namespace userthrift.server
 
         public string GetUser(int userId)
         {
+            //userthrift.itf.model.User user = new userthrift.itf.model.User();
+            //user.UserId = 100;
+            //user.UserName = "test";
+            //user.TrueName = "哈哈哈";
+
+            //// 序列化  
+            //ByteArrayOutputStream out = new ByteArrayOutputStream();
+            //TTransport transport = new (out);
+            //TBinaryProtocol tp = new TBinaryProtocol(transport);//二进制编码格式进行数据传输  
+            //                                                    // TCompactProtocol tp = new TCompactProtocol (transport); 
+
+            //try
+            //{
+            //    user.Write(tp);
+            //}
+            //catch (TException e)
+            //{
+
+            //}
+            //byte[] buf = out.toByteArray();
+            //return buf.ToString();
             return "name of user:" + userId;
         }
 
