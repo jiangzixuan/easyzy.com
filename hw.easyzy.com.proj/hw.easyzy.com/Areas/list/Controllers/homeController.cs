b4e9c726bcd25b90d58a5d6a977070af1b81ef6d
jiang yuanqing <jiang_yuanqing@126.com>
2018-05-21 17:58:46 +0800

    提交

---
+++ hw.easyzy.com.proj/hw.easyzy.com/Areas/list/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hw.easyzy.com.Areas.list.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: list/home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
